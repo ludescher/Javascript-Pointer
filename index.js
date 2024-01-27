f88dc3dd73efdc95b20f59123a7d0c13b00da567
Johannes Ludescher <ludescher.j@gmail.com>
2024-01-27 16:23:58 +0100

    replaced prototype based pointer with functional pointer

--- index.js
+++ index.js
@@ -1,67 +1,79 @@
 var pointer_pool = new Map([[0, { index: 1000000, free: [], id: Symbol("pointer_address") }]]);
 
-if (Number.prototype.hasOwnProperty("getPtrRef") === false) {
-    Number.prototype.getPtrRef = function () {
-        return pointer_pool.get(Number(this));
-    };
+function getPtrRef(ptr) {
+    return pointer_pool.get(ptr);
 }
 
-if (Number.prototype.hasOwnProperty("unlinkPtrRef") === false) {
-    Number.prototype.unlinkPtrRef = function () {
-        const POOL = pointer_pool.get(0);
+function unlinkPtrRef(ptr) {
+    const POOL = pointer_pool.get(0);
 
-        POOL.free.push(this);
+    if (pointer_pool.has(ptr) === true) {
+        delete pointer_pool.get(ptr)[POOL.id];
 
-        return pointer_pool.delete(Number(this));
-    };
+        POOL.free.push(ptr);
+
+        return pointer_pool.delete(ptr);
+    }
+
+    return false;
 }
 
-if (Object.prototype.hasOwnProperty("createPtr") === false) {
-    Object.prototype.createPtr = function () {
-        const POOL = pointer_pool.get(0);
-        let ptr = undefined;
+function createPtr(value) {
+    const POOL = pointer_pool.get(0);
 
-        if (POOL.free.length > 0) {
-            ptr = POOL.free.pop();
-        }
+    if (POOL.id in value) {
+        return value[POOL.id];
+    }
 
-        if (ptr === undefined) {
-            ptr = ++POOL.index;
-        }
+    let ptr = undefined;
 
-        this[POOL.id] = ptr;
+    if (POOL.free.length > 0) {
+        ptr = POOL.free.pop();
+    }
 
-        pointer_pool.set(ptr, this);
+    if (ptr === undefined) {
+        ptr = ++POOL.index;
+    }
 
-        return ptr;
-    };
+    value[POOL.id] = ptr;
+
+    pointer_pool.set(ptr, value);
+
+    return ptr;
 }
 
-if (Object.prototype.hasOwnProperty("unlinkPtr") === false) {
-    Object.prototype.unlinkPtr = function () {
-        const POOL = pointer_pool.get(0);
-        const PTR = this[POOL.id];
+function unlinkPtr(value) {
+    const POOL = pointer_pool.get(0);
 
-        delete this[POOL.id];
+    if ((POOL.id in value) === false) {
+        return false;
+    }
 
+    const PTR = value[POOL.id];
+
+    delete value[POOL.id];
+
+    if (pointer_pool.has(PTR) === true) {
         POOL.free.push(PTR);
 
         return pointer_pool.delete(PTR);
-    };
+    }
+
+    return false;
 }
 
 let user = { name: 'John', age: 32 };
-let user_ptr = user.createPtr();
+let user_ptr = createPtr(user);
 
-console.log({ user_ptr, ref: user_ptr.getPtrRef() });
+console.log({ user_ptr, ref: getPtrRef(user_ptr) });
 
-user.unlinkPtr();
+unlinkPtrRef(user_ptr);
 
 let car = { brand: 'BMW', model: 'M3', manufacture_year: 2003 };
-let car_ptr = car.createPtr();
+let car_ptr = createPtr(car);
 
-console.log({ car_ptr, ref: car_ptr.getPtrRef() });
+console.log({ car_ptr, ref: getPtrRef(car_ptr) });
 
-car.unlinkPtr();
+unlinkPtr(car);
 
-console.log({ car_ptr, ref: car_ptr.getPtrRef() });
+console.log({ car_ptr, ref: getPtrRef(car_ptr) });
